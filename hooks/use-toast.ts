3ef03ca28bf1d3401fd0b44dee1c91e5d3c04fa5
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-11-20 23:30:10 +0330

    Update use-toast.ts

--- hooks/use-toast.ts
+++ hooks/use-toast.ts
@@ -18,3 +18,12 @@ const actionTypes = {
   DISMISS_TOAST: "DISMISS_TOAST",
   REMOVE_TOAST: "REMOVE_TOAST",
 } as const;
+
+let count = 0;
+
+function genId() {
+  count = (count + 1) % Number.MAX_SAFE_INTEGER;
+  return count.toString();
+}
+
+type ActionType = typeof actionTypes;
