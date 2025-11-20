d057eb0d73071dfa4a3269195718b4d932248186
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-11-20 23:30:24 +0330

    Update use-toast.ts

--- hooks/use-toast.ts
+++ hooks/use-toast.ts
@@ -27,3 +27,21 @@ function genId() {
 }
 
 type ActionType = typeof actionTypes;
+
+type Action =
+  | {
+      type: ActionType["ADD_TOAST"];
+      toast: ToasterToast;
+    }
+  | {
+      type: ActionType["UPDATE_TOAST"];
+      toast: Partial<ToasterToast>;
+    }
+  | {
+      type: ActionType["DISMISS_TOAST"];
+      toastId?: ToasterToast["id"];
+    }
+  | {
+      type: ActionType["REMOVE_TOAST"];
+      toastId?: ToasterToast["id"];
+    };
