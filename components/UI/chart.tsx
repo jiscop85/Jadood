2473573ac807043f33354637895031653a0f9dad
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-11-23 01:44:30 +0330

    Update chart.tsx

--- components/UI/chart.tsx
+++ components/UI/chart.tsx
@@ -147,3 +147,20 @@ const [item] = payload;
     if (!active || !payload?.length) {
       return null;
     }
+
+const nestLabel = payload.length === 1 && indicator !== "dot";
+
+    return (
+      <div
+        ref={ref}
+        className={cn(
+          "grid min-w-[8rem] items-start gap-1.5 rounded-lg border border-border/50 bg-background px-2.5 py-1.5 text-xs shadow-xl",
+          className,
+        )}
+      >
+        {!nestLabel ? tooltipLabel : null}
+        <div className="grid gap-1.5">
+          {payload.map((item, index) => {
+            const key = `${nameKey || item.name || item.dataKey || "value"}`;
+            const itemConfig = getPayloadConfigFromPayload(config, item, key);
+            const indicatorColor = color || item.payload.fill || item.color;
