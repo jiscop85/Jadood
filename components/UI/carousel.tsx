8421f31129c81cbbf9326f9c1366f530e19ccdf6
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-11-23 01:41:23 +0330

    Update carousel.tsx

--- components/UI/carousel.tsx
+++ components/UI/carousel.tsx
@@ -11,3 +11,18 @@ type CarouselOptions = UseCarouselParameters[0];
 type CarouselPlugin = UseCarouselParameters[1];
 
 type CarouselProps = {
+ opts?: CarouselOptions;
+  plugins?: CarouselPlugin;
+  orientation?: "horizontal" | "vertical";
+  setApi?: (api: CarouselApi) => void;
+};
+
+type CarouselContextProps = {
+  carouselRef: ReturnType<typeof useEmblaCarousel>[0];
+  api: ReturnType<typeof useEmblaCarousel>[1];
+  scrollPrev: () => void;
+  scrollNext: () => void;
+  canScrollPrev: boolean;
+  canScrollNext: boolean;
+} & CarouselProps;
+
