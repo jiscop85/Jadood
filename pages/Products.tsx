b763b7b2c996160c1c4521352bd05d4b80243d2b
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-11-26 00:10:28 +0330

    Update Products.tsx

---
+++ pages/Products.tsx
@@ -0,0 +1,5 @@
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import ProductCard from "@/components/ProductCard";
+import { Button } from "@/components/ui/button";
+import { Filter } from "lucide-react";
