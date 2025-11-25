409c3ffc05f66e1ef73e6413131fb010ffe48905
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-11-26 00:23:20 +0330

    Update Types.ts

---
+++ integrations_Su/Types.ts
@@ -0,0 +1,25 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
+
+export type Database = {
+  // Allows to automatically instantiate createClient with right options
+  // instead of createClient<Database, { PostgrestVersion: 'XX' }>(URL, KEY)
+  __InternalSupabase: {
+    PostgrestVersion: "13.0.5"
+  }
+  public: {
+    Tables: {
+      cart: {
+        Row: {
+          created_at: string | null
+          id: string
+          product_id: string
+          quantity: number
+          updated_at: string | null
+          user_id: string
+        }
