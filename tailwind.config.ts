e6612cc900d5f5e6070acf32a2429d3a53896c8e
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-11-27 00:55:41 +0330

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -25,3 +25,27 @@ extend: {
           glow: "hsl(var(--primary-glow))",
           dark: "hsl(var(--primary-dark))",
         },
+secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
