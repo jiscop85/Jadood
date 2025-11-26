1f101c3684288b748ea04c8465648dfb48baf98f
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-11-27 00:55:31 +0330

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,3 +12,16 @@ export default {
         "2xl": "1400px",
       },
     },
+extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+          glow: "hsl(var(--primary-glow))",
+          dark: "hsl(var(--primary-dark))",
+        },
