aed01f8c5326d4ff492de6eaea416ef4444d7b8e
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-11-29 21:42:06 +0330

    Update collapsible.tsx

---
+++ components/UI/collapsible.tsx
@@ -0,0 +1,9 @@
+ort * as CollapsiblePrimitive from "@radix-ui/react-collapsible";
+
+const Collapsible = CollapsiblePrimitive.Root;
+
+const CollapsibleTrigger = CollapsiblePrimitive.CollapsibleTrigger;
+
+const CollapsibleContent = CollapsiblePrimitive.CollapsibleContent;
+
+export { Collapsible, CollapsibleTrigger, CollapsibleContent };
