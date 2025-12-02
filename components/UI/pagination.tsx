b19738cc1e81c0d7f15591376a33df2c70613fae
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-12-02 23:02:55 +0330

    Update pagination.tsx

--- components/UI/pagination.tsx
+++ components/UI/pagination.tsx
@@ -30,3 +30,18 @@ type PaginationLinkProps = {
   isActive?: boolean;
 } & Pick<ButtonProps, "size"> &
   React.ComponentProps<"a">;
+
+const PaginationLink = ({ className, isActive, size = "icon", ...props }: PaginationLinkProps) => (
+  <a
+    aria-current={isActive ? "page" : undefined}
+    className={cn(
+      buttonVariants({
+        variant: isActive ? "outline" : "ghost",
+        size,
+      }),
+      className,
+    )}
+    {...props}
+  />
+);
+PaginationLink.displayName = "PaginationLink";
