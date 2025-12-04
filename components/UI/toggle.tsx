06dc80d915d11512af7192e6f4c9852dfbf0afdc
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-12-04 22:35:15 +0330

    Update toggle.tsx

--- components/UI/toggle.tsx
+++ components/UI/toggle.tsx
@@ -24,3 +24,14 @@ const toggleVariants = cva(
     },
   },
 );
+
+onst Toggle = React.forwardRef<
+  React.ElementRef<typeof TogglePrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof TogglePrimitive.Root> & VariantProps<typeof toggleVariants>
+>(({ className, variant, size, ...props }, ref) => (
+  <TogglePrimitive.Root ref={ref} className={cn(toggleVariants({ variant, size, className }))} {...props} />
+));
+
+Toggle.displayName = TogglePrimitive.Root.displayName;
+
+export { Toggle, toggleVariants };
